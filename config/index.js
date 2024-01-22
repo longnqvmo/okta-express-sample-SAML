9c8352057591f12d44e726da33ee88b1f25025a8
long <longnq@vmogroup.com>
2024-01-22 09:40:54 +0700

    empty config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const config = {
 	saml: {
 		cert: 'config/saml.pem',
-		entryPoint: 'https://dev-24943231.okta.com/home/dev-24943231_samltestapp_1/0oael5jc40UD9Dyg15d7/alnel5la5tMHkYiWQ5d7',
+		entryPoint: '',
 		issuer: 'http://localhost:1337',
 		options: {
 			failureRedirect: '/login',
@@ -10,11 +10,6 @@ const config = {
 	},
 	server: {
 		port: 1337,
-	},
-	session: {
-		resave: false,
-		secret: 'supersecretamazingpassword',
-		saveUninitialized: true,
 	},
 };
 
